668ed6a0f100e75c221e129ea1486c3bb1ee1a6a
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-09-15 19:35:18 +0900

    Merge branch 'develop' into main
    - ci!: add a step to test this module
    - test!: add program to check this module
    - fix: adjust version: fortran-lang/setup-fpm
    - fix: add setting: fpm-version for fortran-lang/setup-fpm
    - ci!: add a workflow file for CI
    - feat!: add implementation: integrate_inv_r_on_rectangle
    - ci: add manifest file for fpm
    - docs: update description about this module

---
+++ test/check.f90
@@ -0,0 +1,32 @@
+program check
+
+    use, intrinsic :: iso_fortran_env , only: real64
+
+    use, non_intrinsic :: integrate_inv_r_on_rectangle_fortran , only: integrate => integrate_inv_r_on_rectangle
+
+
+
+    implicit none
+
+
+
+    print *, '  7.0509886961563442018608745998383384722'
+    print *, integrate( inf_x = -1.0_real64 , sup_x = 1.0_real64 , inf_y = -1.0_real64 , sup_y = 1.0_real64 )
+
+    print *
+
+    print *, '  3.5254943480781721009304372999191692361'
+    print *, integrate( inf_x =  0.0_real64 , sup_x = 1.0_real64 , inf_y = -1.0_real64 , sup_y = 1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x = 1.0_real64 , inf_y =  0.0_real64 , sup_y = 1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x = 0.0_real64 , inf_y = -1.0_real64 , sup_y = 1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x = 1.0_real64 , inf_y = -1.0_real64 , sup_y = 0.0_real64 )
+
+    print *
+
+    print *, '  1.7627471740390860504652186499595846181'
+    print *, integrate( inf_x =  0.0_real64 , sup_x = 1.0_real64 , inf_y =  0.0_real64 , sup_y = 1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x = 0.0_real64 , inf_y =  0.0_real64 , sup_y = 1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x = 0.0_real64 , inf_y = -1.0_real64 , sup_y = 0.0_real64 )
+    print *, integrate( inf_x =  0.0_real64 , sup_x = 1.0_real64 , inf_y = -1.0_real64 , sup_y = 0.0_real64 )
+
+end program check
