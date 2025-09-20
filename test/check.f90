2b3c69c5fd1a63dd6a1bcc2bdd5bbb5c172bff4d
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-09-20 10:27:34 +0900

    test: add test targets - [+0.5, +1.0, -1.0, +1.0] - [-1.0, +1.0, +0.5, +1.0] - [-1.0, -0.5, -1.0, +1.0] - [-1.0, +1.0, -1.0, -0.5]

--- test/check.f90
+++ test/check.f90
@@ -18,6 +18,14 @@ program check
 
     print *
 
+    print *, '  1.1194352227801548634342191520109696563'
+    print *, integrate( inf_x =  0.5_real64 , sup_x =  1.0_real64 , inf_y = -1.0_real64 , sup_y =  1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x =  1.0_real64 , inf_y =  0.5_real64 , sup_y =  1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x = -0.5_real64 , inf_y = -1.0_real64 , sup_y =  1.0_real64 )
+    print *, integrate( inf_x = -1.0_real64 , sup_x =  1.0_real64 , inf_y = -1.0_real64 , sup_y = -0.5_real64 )
+
+    print *
+
     print *, '  7.0509886961563442018608745998383384722'
     print *, integrate( inf_x = -1.0_real64 , sup_x = 1.0_real64 , inf_y = -1.0_real64 , sup_y = 1.0_real64 )
 
